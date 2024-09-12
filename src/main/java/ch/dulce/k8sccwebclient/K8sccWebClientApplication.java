5088c0066e076ea1358500d17674de370d214e47
Antonio Dolce <dolce.antonio@gmail.com>
2024-09-12 22:46:47 +0200

    removed config client

--- src/main/java/ch/dulce/k8sccwebclient/K8sccWebClientApplication.java
+++ src/main/java/ch/dulce/k8sccwebclient/K8sccWebClientApplication.java
@@ -2,9 +2,7 @@ package ch.dulce.k8sccwebclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class K8sccWebClientApplication {
 
