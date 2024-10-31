770449e5be150d4adb6643b7695d227a1dac3634
Antonio Dolce <dolce.antonio@gmail.com>
2024-10-31 22:51:05 +0100

    fixed Controller mapping

--- src/main/java/ch/dulce/k8sccwebclient/controller/MyController.java
+++ src/main/java/ch/dulce/k8sccwebclient/controller/MyController.java
@@ -3,9 +3,11 @@ package ch.dulce.k8sccwebclient.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RequestMapping("/")
+@RestController()
 public class MyController {
 
   @Value("${app.greeting}")
