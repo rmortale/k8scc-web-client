e76b29e6ca8d6f8c33c9be0ade8fabecd40cda1a
Antonio Dolce <dolce.antonio@gmail.com>
2024-11-17 00:31:23 +0100

    new tag strategy

--- k8s/delete-feature-to-k0s.sh
+++
@@ -1,5 +0,0 @@
-kubectx k0s
-kubens k8scc-pr
-
-helm delete pr-test
-
--- src/main/java/ch/dulce/k8sccwebclient/controller/MyController.java
+++ src/main/java/ch/dulce/k8sccwebclient/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping(path = "hello")
 @RestController()
 public class MyController {
 
